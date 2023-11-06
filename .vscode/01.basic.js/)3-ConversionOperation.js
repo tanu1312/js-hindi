d5f26aa1bcf0c55c3e243f1c721b574aa5905edb
Tanu Yadav <144536504+tanu1312@users.noreply.github.com>
2023-11-06 19:19:36 +0000

    Continue js

---
+++ .vscode/01.basic.js/)3-ConversionOperation.js
@@ -0,0 +1,28 @@
+let score =true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abx" => NaN
+//true =>1; false=>0
+
+let isLoggedin = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedin)
+// console.log( booleanIsLoggedIn);
+
+// 1 =>true ; 0=> false
+//"" => false
+//"tanu"=> true
+
+let someNumber =33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
---
+++ .vscode/01.basic.js/02-datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+ //alert("hello")
+// alert( 3 + 3 ) //we are using node.js, not browser
+
+console.log(3
+     +
+     3) //code readability should be high
+console.log("tanu")
+
+
+
+
+// number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// objects
+
+
+console.log(typeof null);
