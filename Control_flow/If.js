3372d6e4066f891611c704b6e1267af9474f767e
Tanu Yadav <144536504+tanu1312@users.noreply.github.com>
2024-01-01 23:00:29 +0530

    if statement

---
+++ Control_flow/If.js
@@ -0,0 +1,31 @@
+//If Statements
+const isUserLoggedIn =true
+const temperature =41
+
+// if(temperature == 41){
+//     console.log("executed");
+// }
+
+// console.log{"temperature is greater than 50"}
+
+
+
+if(temperature == 41){
+    console.log("executed");
+}else{
+
+console.log{"temperature is greater than 50"}
+}
+console.log("executed");
+
+
+// const scrore
+
+// if(score > 100) {
+//     const power ="fly"
+//     console.log(`User power: ${power}`)
+// }
+// console.log(`User power: ${power}`)
+
+const balance =100
+if(balance >500) console.log("test");
